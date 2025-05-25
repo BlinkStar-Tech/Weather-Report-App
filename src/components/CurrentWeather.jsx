a999c642942ea49b8768a813902f04ff40f3d335
BlinkStar-Tech <blinkstardesigns@gmail.com>
2025-05-25 16:45:02 +0200

    added an icon

--- src/components/CurrentWeather.jsx
+++ src/components/CurrentWeather.jsx
@@ -1,12 +1,17 @@
 const CurrentWeather = ({ currentWeather }) => {
-    return (
-      <div className="current-weather">
-        <img src={`icons/${currentWeather.weatherIcon}.svg`} className="weather-icon" />
-        <h2 className="temperature">
-          {currentWeather.temperature} <span>°C</span>
-        </h2>
-        <p className="description">{currentWeather.description}</p>
-      </div>
-    );
-  };
-  export default CurrentWeather;
+  return (
+    <div className="current-weather">
+      <img 
+        src={`src/images/clouds-and-sun.png`} // Updated to point to weather.jpg
+        className="weather-icon" 
+        alt={currentWeather.description} 
+      />
+      <h2 className="temperature">
+        {currentWeather.temperature} <span>°C</span>
+      </h2>
+      <p className="description">{currentWeather.description}</p>
+    </div>
+  );
+};
+
+export default CurrentWeather;
