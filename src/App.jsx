1ad2a543416b2422391ccd7a28a6c2e561f5586c
BlinkStar-Tech <blinkstardesigns@gmail.com>
2025-05-25 15:36:50 +0200

    current location changed

--- src/App.jsx
+++ src/App.jsx
@@ -44,7 +44,7 @@ const App = () => {
   };
   // Fetch default city (London) weather data on initial render
   useEffect(() => {
-    const defaultCity = "London";
+    const defaultCity = "Harare";
     const API_URL = `https://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${defaultCity}&days=2`;
     getWeatherDetails(API_URL);
   }, []);
